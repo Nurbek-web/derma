cdc5c78dfcf5b76225a56f8e72221813edce8753
Nurbek-web <taizhanovntm@gmail.com>
2023-02-18 12:29:15 +0600

    commit

--- jscript/app_startup_code.js
+++ jscript/app_startup_code.js
@@ -33,8 +33,8 @@ function predictOnLoad() {
 let model;
 (async function () {
 	
-	model = await tf.loadModel('https://teal-marshmallow-392fb8.netlify.app/final_model_kaggle_version1/model.json');
-	$("#selected-image").attr("src", "https://teal-marshmallow-392fb8.netlify.app/assets/samplepic.jpg");
+	model = await tf.loadModel('https://dermanis.netlify.app//final_model_kaggle_version1/model.json');
+	$("#selected-image").attr("src", "https://dermanis.netlify.app//assets/samplepic.jpg");
 	
 	// Hide the model loading spinner
 	// This line of html gets hidden:
